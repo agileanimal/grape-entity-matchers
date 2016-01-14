ecb055dbfce92b3a81f6eaa9c91d7b57ad4e0eb2
Tim Perkins <tperkins@salsify.com>
2016-01-14 12:52:32 -0500

    Add DocumentMatcher
    This matcher allows expectations to be checked on the documentation
    for an exposure.

--- lib/grape_entity_matchers/rspec_integration.rb
+++ lib/grape_entity_matchers/rspec_integration.rb
@@ -3,4 +3,6 @@ require 'rspec'
 RSpec.configure do |config|
   require 'grape_entity_matchers/represent_matcher'
   config.include GrapeEntityMatchers::RepresentMatcher
+  require 'grape_entity_matchers/document_matcher'
+  config.include GrapeEntityMatchers::DocumentMatcher
 end
