43dea6d1327d86e97fe54242296728cd05e71e5c
Tim Perkins <tperkins@salsify.com>
2017-04-28 15:34:01 -0400

    Support for grape-entity >= 0.5.0

--- lib/grape_entity_matchers.rb
+++ lib/grape_entity_matchers.rb
@@ -1,4 +1,5 @@
 require 'grape_entity_matchers/version'
+require 'grape_entity_matchers/exposures'
 require 'grape_entity_matchers/represent_matcher'
 require 'grape_entity_matchers/document_matcher'
 require 'grape_entity_matchers/rspec_integration'
